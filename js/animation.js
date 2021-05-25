4ce55eb8e93beedb89352881e33bd2c719e8ff43
unknown <FranckTshiz27>
2021-05-25 12:36:13 +0100

    refactor : correct navBar

--- js/animation.js
+++ js/animation.js
@@ -1,39 +1,44 @@
-
 let mainMenu;
 let openMenu;
 let closeMenu;
+let btnMenu;
+let nav;
+let isVisible = false;
 initComponents();
-function initComponents()
-{
-    mainMenu = document.querySelector(".mainMenu");
-    openMenu = document.querySelector(".openMenu");
-    closeMenu = document.querySelector(".closeMenu");
-
+function initComponents() {
+  mainMenu = document.querySelector(".mainMenu");
+  openMenu = document.querySelector(".openMenu");
+  closeMenu = document.querySelector(".closeMenu");
+  btnMenu = document.querySelector(".navbar-toggler");
+  nav = document.querySelector("#navbarTogglerDemo01");
 }
 
+openMenu.addEventListener("click", show);
 
-openMenu.addEventListener('click',show);
-closeMenu.addEventListener('click',close);
+openMenu.addEventListener("click", show);
+closeMenu.addEventListener("click", close);
 
-function show()
-{
-    
-    mainMenu.style.display='flex';
-    mainMenu.style.top='0';
+function show() {
+  isVisible = !isVisible;
 
+  if (isVisible) {
+    mainMenu.style.display = "flex";
+    mainMenu.style.top = "0";
+  } else {
+    mainMenu.style.display = "none";
+  }
 }
- 
 
-function close()
-{
-    
-    mainMenu.style.top='-100%';
-
-} 
+function close() {
+  mainMenu.style.top = "-100%";
+}
 
 let hash = window.location.hash;
- let aLinkActualHash = document.querySelector('a[href="' + hash +'"]') ;
-  if(aLinkActualHash !== null && !aLinkActualHash.parentNode.classList.contains("active"))
-  { console.log("hash")
-    showTabs(aLinkActualHash)
-  }
+let aLinkActualHash = document.querySelector('a[href="' + hash + '"]');
+if (
+  aLinkActualHash !== null &&
+  !aLinkActualHash.parentNode.classList.contains("active")
+) {
+  console.log("hash");
+  showTabs(aLinkActualHash);
+}
